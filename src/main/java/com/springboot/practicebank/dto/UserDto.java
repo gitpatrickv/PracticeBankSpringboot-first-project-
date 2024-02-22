a0497d77d59902ad82ca68099589e836dc887a19
rektgit2024 <myemailispatrick@gmail.com>
2024-02-22 08:06:40 +0800

    @Test commit

--- src/main/java/com/springboot/practicebank/dto/UserDto.java
+++ src/main/java/com/springboot/practicebank/dto/UserDto.java
@@ -26,24 +26,24 @@ import java.math.BigDecimal;
 @Builder
 public class UserDto {
 
-    @NotBlank(groups = OnCreate.class)
+    @NotBlank(groups = OnCreate.class, message = "{firstname.required}")
     private String firstName;
-    @NotBlank(groups = OnCreate.class)
+    @NotBlank(groups = OnCreate.class, message = "{lastname.required}")
     private String lastName;
     @Enumerated(EnumType.STRING)
     private Gender gender;
-    @NotBlank(groups = OnCreate.class)
+    @NotBlank(groups = OnCreate.class, message = "{address.required}")
     private String address;
-    @NotBlank(groups = OnCreate.class)
+    @NotBlank(groups = OnCreate.class, message = "{phone.number.required}")
     private String phoneNumber;
     @AgeValid(groups = OnCreate.class)
     private Integer age;
     @UniqueEmailValid(groups = {OnCreate.class, OnUpdate.class})
     @Email(groups = {OnCreate.class, OnUpdate.class})
-    @NotBlank(groups = {OnCreate.class, OnUpdate.class})
+    @NotBlank(groups = {OnCreate.class, OnUpdate.class}, message = "{email.required}")
     private String email;
 
-    @NotBlank(groups = OnCreate.class)
+    @NotBlank(groups = OnCreate.class, message = "{password.required}")
     private String password;
 
     private String accountNumber;
@@ -52,8 +52,6 @@ public class UserDto {
     @Size(min = 4, max = 4, message = "{pin.number.invalid}", groups = OnCreate.class)
     @Pattern(regexp = "\\d{4}", message = "{pin.number.size}", groups = OnCreate.class)
     private String atmPin;
-
-
 
     @Enumerated(EnumType.STRING)
     private Status status;
